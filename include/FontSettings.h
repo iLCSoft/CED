f89a74e42a3a8d8cb76733c54dfe188a83229d2c
Victor Schwan <victor.schwan@cern.ch>
2025-06-03 17:44:48 +0200

    turn getFontDimensions into header-only function

--- include/FontSettings.h
+++ include/FontSettings.h
@@ -4,8 +4,17 @@
 struct FontDimensions {
     int height;
     int width;
+
+    constexpr FontDimensions(int h, int w) : height(h), width(w) {}
 };
 
-FontDimensions getFontDimensions(int fontSetting);
+inline constexpr FontDimensions getFontDimensions(int fontSetting) {
+    switch (fontSetting) {
+        case 0: return {10, 6};
+        case 1: return {12, 8};
+        case 2: return {20, 11};
+        default: return {10, 2};
+    }
+}
 
 #endif
